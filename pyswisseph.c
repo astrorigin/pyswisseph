778903d59bed84b8da020cee77f1995b0df5106b
Stanislas Marquis <stan@astrorigin.com>
2023-06-04 18:44:42 +0200

    Prepare release v2.10.03.2

--- pyswisseph.c
+++ pyswisseph.c
@@ -32,7 +32,7 @@
  *  Swisseph version: 2.10.03
  */
 
-#define PYSWISSEPH_VERSION      20220914
+#define PYSWISSEPH_VERSION      20230604
 
 /* Set the default argument for set_ephe_path function */
 #ifndef PYSWE_DEFAULT_EPHE_PATH
