a7d191ab886c349b944d691e477f76cdf68c5ebb
sailorfe <sailorfe@proton.me>
2025-10-09 14:26:50 -0400

    docs-build: adding mocking to docs/conf.py and recursive submodules to workflow

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
+autodoc_mock_imports = ['swisseph']
 
 project = 'Pyswisseph'
 copyright = '2007-2025, Pyswisseph authors and contributors'
